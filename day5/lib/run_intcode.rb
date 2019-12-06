38a0196b13744d4ab69ac702d829be9590e88017
Mavenlink <mavenlink@MacBook-Pro.local>
2019-12-05 18:43:38 -0700

    refactor day 5

--- day5/lib/run_intcode.rb
+++ day5/lib/run_intcode.rb
@@ -4,67 +4,88 @@ class RunIntcode
     @input = input
     @output = nil
     @invalid_opcode = nil
+    @pointer = 0
   end
-  INSTRUCTIONS_PARAMS = {
-    1 => 3,
-    2 => 3,
-    3 => 1,
-    4 => 1,
-    5 => 2,
-    6 => 2,
-    7 => 3,
-    8 => 3
+
+  INSTRUCTIONS = {
+    1 => 'add',
+    2 => 'multiply',
+    3 => 'write_input',
+    4 => 'output',
+    5 => 'jump_if_true',
+    6 => 'jump_if_false',
+    7 => 'less_than',
+    8 => 'equal_to',
+    99 => 'skip'
   }
 
   def run_program
-    pointer = 0
-    until false
-      instructions = @program[pointer].digits
-      opcode = instructions.last
-      # raise ArgumentError if @output && opcode != 9
-      num_params = INSTRUCTIONS_PARAMS[opcode]
-      modified_pointer = false
-      case opcode
-      when 1
-        @program[@program[pointer+3]] = get_param(instructions[-3], @program[pointer+1]) + get_param(instructions[-4], @program[pointer+2])
-      when 2
-        @program[@program[pointer+3]] = get_param(instructions[-3], @program[pointer+1]) * get_param(instructions[-4], @program[pointer+2])
-      when 3
-        @program[@program[pointer+1]] = @input
-      when 4
-        @output = @program[@program[pointer+1]]
-        @invalid_opcode = @program[pointer]
-      when 5
-        if(get_param(instructions[-3], @program[pointer+1]) != 0)
-          modified_pointer = true
-          pointer = get_param(instructions[-4], @program[pointer+2])
-        end
-      when 6
-        if(get_param(instructions[-3], @program[pointer+1]) == 0)
-          modified_pointer = true
-          pointer = get_param(instructions[-4], @program[pointer+2])
-        end
-      when 7
-        @program[@program[pointer+3]] = (get_param(instructions[-3], @program[pointer+1]) < get_param(instructions[-4], @program[pointer+2])) ? 1 : 0
-      when 8
-        @program[@program[pointer+3]] = (get_param(instructions[-3], @program[pointer+1]) == get_param(instructions[-4], @program[pointer+2])) ? 1 : 0
-      when 9
-        return @output
-      end
-      pointer += (num_params + 1) unless modified_pointer
+    opcode = 0
+    until opcode == 99
+      instructions = @program[@pointer].digits
+      opcode = instructions.last(2).join.to_i
+      @modes = instructions[0...-2]
+      self.send(INSTRUCTIONS[opcode])
     end
-    @program
+    @output
   end
 
-  def get_param(mode, param)
-    case mode
-    when 0
-      @program[param]
-    when 1
-      param
-    else
-      @program[param]
-    end
+  def add
+    set(3, param(1) + param(2))
+    move_pointer_by(4)
+  end
+
+  def multiply
+    set(3, param(1) * param(2))
+    move_pointer_by(4)
+  end
+
+  def write_input
+    set(1, @input)
+    move_pointer_by(2)
+  end
+
+  def output
+    @output = param(1)
+    move_pointer_by(2)
+  end
+
+  def jump_if_true
+    param(1) != 0 ? move_pointer_to(param(2)): move_pointer_by(3)
+  end
+
+  def jump_if_false
+    param(1) == 0 ? move_pointer_to(param(2)): move_pointer_by(3)
+  end
+
+  def skip
+  end
+
+  def less_than
+    set(3, (param(1) < param(2)) ? 1 : 0)
+    move_pointer_by(4)
+  end
+
+  def equal_to
+    set(3, (param(1) == param(2)) ? 1 : 0)
+    move_pointer_by(4)
+  end
+
+  def move_pointer_by(distance)
+    @pointer += distance
+  end
+
+  def move_pointer_to(location)
+    @pointer = location
+  end
+
+  def param(position)
+    param = @program[@pointer + position]
+    @modes[-position] == 1 ? param : @program[param]
+  end
+
+  def set(position, value)
+    @program[@program[@pointer+position]] = value
   end
 end
 
